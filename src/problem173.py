49bf07d6de63fb7239a3d970ad0e6ee6adea05c3
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2022-01-07 19:05:13 -0300

    solve problem 173
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ src/problem173.py
@@ -0,0 +1,55 @@
+"""
+This problem was asked by Stripe. (EASY)
+
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+
+You can assume keys do not contain dots in them, i.e. no clobbering will occur.
+"""
+
+
+def flat_dictionary_helper(root_key, dictionary, flatten):
+    for key in dictionary.keys():
+        new_key = key
+
+        if root_key != "":
+            new_key = root_key + "." + key
+
+        if type(dictionary[key]) is dict:
+            flat_dictionary_helper(new_key, dictionary[key], flatten)
+        else:
+            flatten[new_key] = dictionary[key]
+
+
+def flat_dictionary(dictionary):
+    flatten_dict = dict()
+    flat_dictionary_helper("", dictionary, flatten_dict)
+    return flatten_dict
+
+
+"""
+SOLUTION:
+n = total of keys in dictionary and nested dictionary
+
+- time complexity: O(n)
+- space complexity: O(n)
+"""
